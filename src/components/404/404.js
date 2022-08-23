50668b429b3a50a45d7a04fef81f3d7fc967b1b0
NhanNguyen <64063604+nguyentrongnhan1412@users.noreply.github.com>
2022-08-23 23:45:30 +0700

    first commit

---
+++ src/components/404/404.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./404.css";
+
+const NoMatch = () => {
+  return (
+    <>
+      <h1 className="no-match">
+        <strong>404 Not Found</strong>
+      </h1>
+    </>
+  );
+};
+
+export default NoMatch;
