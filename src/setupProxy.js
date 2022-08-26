669056bca4454b92c97ca32a68b4f69ee0bec26e
pear_ngoc <lethanhngoc123a@gmail.com>
2022-08-26 13:15:38 +0700

    new statistic

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,5 +4,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(createProxyMiddleware("/api/v1/location/admin/statistic/driver", { target: "http://localhost:8080" }));
+    app.use(createProxyMiddleware("/v1/statistic", { target: "http://34.172.47.16:8080" }));
 };
 
